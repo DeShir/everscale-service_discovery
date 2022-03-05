bdd31dc3b0d5c22c95868d2a4afce13d1279e730
Denis Shirokov <de.sh.vit@gmail.com>
2022-03-06 00:41:51 +0300

    fix issues

--- src/main/solidity/service/ServiceDiscoveryClient.sol
+++ src/main/solidity/service/ServiceDiscoveryClient.sol
@@ -3,18 +3,40 @@ pragma ton-solidity >= 0.53.0;
 import "./IServiceDiscovery.sol";
 
 abstract contract ServiceDiscoveryClient {
-    private optinal(address) addr;
+    optional(address) private addr;
 
     function setAddress(address _addr) external {
         tvm.accept();
         addr = _addr;
     }
 
-    function find(string[] tags) public returns(address[]) {
-        return IServiceDiscovery(addr).find(tags);
+    function find(string[] tags) public {
+        require(addr.hasValue(), 100);
+        optional(uint256) none;
+        return IServiceDiscovery(addr.get()).find{
+            sign: false,
+            pubkey: none,
+            time: uint64(now),
+            expire: 0,
+            callbackId: tvm.functionId(findCallback),
+            onErrorId: 0
+        }(tags).extMsg;
     }
 
-    function all() public returns(Service[]) {
-        return IServiceDiscovery(addr).all();
+    function findCallback(address[] addrs) public virtual;
+
+    function all() public {
+        require(addr.hasValue(), 100);
+        optional(uint256) none;
+        return IServiceDiscovery(addr.get()).all{
+            sign: false,
+            pubkey: none,
+            time: uint64(now),
+            expire: 0,
+            callbackId: tvm.functionId(allCallback),
+            onErrorId: 0
+        }().extMsg;
     }
+
+    function allCallback(IServiceDiscovery.Service[] services) public virtual;
 }
